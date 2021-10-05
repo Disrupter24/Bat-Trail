8a6107bae4c99bbbe5da60d317938efdd700e944
Disrupter24 <noahmacdonald13@gmail.com>
2021-10-05 17:12:07 -0400

    Reorganized, implemented fruit and pitfalls
    'nuff said

--- Bat Trail/Assets/Scripts/MapPreparation.cs
+++ Bat Trail/Assets/Scripts/MapPreparation.cs
@@ -9,22 +9,24 @@ public class MapPreparation : MonoBehaviour
     public static int BottomBorder;
     public static int RightBorder;
     private SpriteRenderer GridSpriteRenderer;
-    public bool LoadPreset = true;
     public static Vector2[] WallLocations;
     public static Vector2[] FruitLocations;
-    public static Vector2[] HoleLocations;
+    public static Vector2[] PitfallLocations;
     private int WallCount = 3;
     private Object WallPrefab;
+    private Object FruitPrefab;
+    private Object PitfallPrefab;
     void Start()
     {
         SetBorders(); //NOTE: This script will assume that the box is perfectly rounded to the grid and centered on 0, otherwise some jank might occur.
         WallHandler();
+        FruitHandler();
+        PitfallHandler();
     }
 
     private void SetBorders()
     {
         GridSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        // GridSpriteRenderer.enabled = false;
         TopBorder = Mathf.RoundToInt(0.5f + transform.position.y + (GridSpriteRenderer.size.y / 2));
         Debug.Log(TopBorder);
         LeftBorder = Mathf.RoundToInt(-0.5f + transform.position.x - (GridSpriteRenderer.size.x / 2));
@@ -37,20 +39,26 @@ public class MapPreparation : MonoBehaviour
     private void WallHandler()
     {
         WallPrefab = Resources.Load("prefabs/Wall", typeof(GameObject));
-        WallLocations = new Vector2[WallCount];
-        if (LoadPreset) // Contains the locations of preset walls in the first map layout
-        {
-            WallLocations[0] = new Vector2(4, 4);
-            WallLocations[1] = new Vector2(1, 1);
-            WallLocations[2] = new Vector2(2, 2);
-        }
-        SpawnTime(WallLocations);
+        SpawnTime(WallLocations, WallPrefab);
     }
-    void SpawnTime(Vector2[] SpawnType)
+    private void FruitHandler()
     {
-        for (int i = 0; i < SpawnType.Length; i++)
+        FruitPrefab = Resources.Load("prefabs/Fruit", typeof(GameObject));
+        SpawnTime(FruitLocations, FruitPrefab);
+    }
+    private void PitfallHandler()
+    {
+        PitfallPrefab = Resources.Load("prefabs/Pitfall", typeof(GameObject));
+        SpawnTime(PitfallLocations, PitfallPrefab);
+    }
+    void SpawnTime(Vector2[] SpawnType, Object Prefab)
+    {
+        if(SpawnType != null && Prefab!= null)
         {
-            Instantiate(WallPrefab, SpawnType[i], Quaternion.identity);
+            for (int i = 0; i < SpawnType.Length; i++)
+            {
+                Instantiate(Prefab, SpawnType[i], Quaternion.identity);
+            }
         }
     }
 }
